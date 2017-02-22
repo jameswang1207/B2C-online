dc5177d6948d01dd6ba629f3e4e4301992be9c62
jameswang <1983297642@qq.com>
2017-02-22 20:09:30 +0800

    init project manager

---
+++ b2c-online-backend-manager/b2c-manager-mapper/src/main/java/com/b2c/mapper/TbItemMapper.java
@@ -0,0 +1,30 @@
+package com.b2c.mapper;
+
+import com.b2c.pojo.TbItem;
+import com.b2c.pojo.TbItemExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbItemMapper {
+    int countByExample(TbItemExample example);
+
+    int deleteByExample(TbItemExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbItem record);
+
+    int insertSelective(TbItem record);
+
+    List<TbItem> selectByExample(TbItemExample example);
+
+    TbItem selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbItem record, @Param("example") TbItemExample example);
+
+    int updateByExample(@Param("record") TbItem record, @Param("example") TbItemExample example);
+
+    int updateByPrimaryKeySelective(TbItem record);
+
+    int updateByPrimaryKey(TbItem record);
+}
